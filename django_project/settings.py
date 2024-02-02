3de82fc87a9bdb6907b798af0766332800b713e9
Chris Walls <chris@absolutegis.com>
2024-02-01 23:22:27 -0500

    updated settings v2

--- django_project/settings.py
+++ django_project/settings.py
@@ -186,7 +186,7 @@ DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 
 
 
-# Default primary key field type
+# Default primary key field type - d
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
